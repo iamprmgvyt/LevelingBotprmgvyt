76f75fd6555ff341ce4fd6c8d7e10d41911e80cb
Gia Bao Nguyen <thuvien040281@gmail.com>
2025-12-17 18:09:01 +0700

    Refactor passport user serialization and deserialization

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -1,14 +1,8 @@
 const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
-const { getGuildConfig } = require('../utils/database');
 
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
-
-passport.deserializeUser((obj, done) => {
-    done(null, obj);
-});
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
 
 passport.use(new DiscordStrategy({
     clientID: process.env.DISCORD_CLIENT_ID,
@@ -16,6 +10,5 @@ passport.use(new DiscordStrategy({
     callbackURL: `${process.env.DASHBOARD_URL}/auth/discord/callback`,
     scope: ['identify', 'guilds']
 }, (accessToken, refreshToken, profile, done) => {
-    // This passes the user profile to the session
     return done(null, profile);
 }));
