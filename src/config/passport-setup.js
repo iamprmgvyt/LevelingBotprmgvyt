99d35aa989c5d7a6c6421f9dc2645fcafe06dd9a
Gia Bao Nguyen <thuvien040281@gmail.com>
2025-12-17 19:54:47 +0700

    Update passport-setup.js

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -7,8 +7,9 @@ passport.deserializeUser((obj, done) => done(null, obj));
 passport.use(new DiscordStrategy({
     clientID: process.env.DISCORD_CLIENT_ID,
     clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    callbackURL: `${process.env.DASHBOARD_URL}/auth/discord/callback`,
+    callbackURL: process.env.CALLBACK_URL || 'https://levelingbotprmgvyt.onrender.com/auth/discord/callback',
     scope: ['identify', 'guilds']
 }, (accessToken, refreshToken, profile, done) => {
+    // We simply pass the profile to the session
     return done(null, profile);
 }));
