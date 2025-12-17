a76c488e36af940fae56293ec9c0bd0eaef06ba2
Gia Bao Nguyen <thuvien040281@gmail.com>
2025-12-17 19:59:22 +0700

    Update auth.js

--- src/web/routes/auth.js
+++ src/web/routes/auth.js
@@ -1,18 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const passport = require('passport');
 
-// This starts the Discord login process
+// This name "discord" must match the strategy name
 router.get('/discord', passport.authenticate('discord'));
 
-// This is where Discord sends the user back after they click "Authorize"
 router.get('/discord/callback', passport.authenticate('discord', {
     failureRedirect: '/'
 }), (req, res) => {
-    res.redirect('/'); // Successful login, send them back to home
+    res.redirect('/dashboard');
 });
 
-// Logout route
 router.get('/logout', (req, res) => {
     req.logout(() => {
         res.redirect('/');
